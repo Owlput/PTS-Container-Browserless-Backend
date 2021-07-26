e7c7e57bd7c064d13b6e4d77987b58a1fd71b1de
Dr_Huang_MHT <59129019+drHuangMHT@users.noreply.github.com>
2021-07-26 22:25:05 +0800

    [UPD]:Made reading config.json possible

--- Browserless-Backend/config/configRegister.js
+++ Browserless-Backend/config/configRegister.js
@@ -1,22 +1,18 @@
 const fs = require("fs");
-module.exports = function getConfigJSON() {
-  try {
-    //Try to read config.json for configurations and return an object
-    () => {
-      JSON.parse(fs.readFileSync(config.json));
-      return config;
-    };
-    console.log("Successfully loaded config.json as configurations.")
-  } catch (err) {
-    console.err(
-      "Failed to load config file! \
-        Is there something wrong with the config.json or the file is missing?\
-        Now use default values instead."
-    )(() => {
-      let defaultConfig = {
-        port: 3000,
-      };
-      return defaultConfig;
-    });
+exports.blbeGetConfig = function() {
+    let configObject;
+    try {
+      configObject = JSON.parse(fs.readFileSync("./Browserless-Backend/config/config.json", "utf-8"));
+    } catch (err) {
+      console.error(err);
+    }
+    finally{
+      if (configObject instanceof Object){
+        return configObject
+      } else {
+        return {
+          "port":3000
+        }
+      }
+    }
   }
-};
--- Browserless-Backend/main.js
+++ Browserless-Backend/main.js
@@ -1,42 +1,14 @@
 const net = require("net");
 const readline = require("readline");
+const confighandler = require("./config/configRegister.js");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let globalConfig = confighandler.blbeGetConfig()
 
 /*----------------------------------------*/
 /* Start Browserless Backend(BLBE) server */
 /*----------------------------------------*/
 let blbe = net.createServer();
-blbe.listen(50000, () => console.log("Listening on port 50000"));// Listening connections
+blbe.listen(globalConfig.port, () => console.log(`Listening on port ${globalConfig.port}`));// Listening connections
 blbe.on("connection", (socket) => {
   inputListener(socket)//Process incoming requests
     .then(() => socket.end)

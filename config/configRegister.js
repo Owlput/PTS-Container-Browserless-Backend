b06ae098584e84d255b638455a51a6e0424a2bad
Dr_Huang_MHT <59129019+drHuangMHT@users.noreply.github.com>
2021-10-27 23:09:19 +0800

    重构

--- config/configRegister.js
+++ config/configRegister.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 exports.blbeGetConfig = function() {
     let configObject
     try {
-      configObject = JSON.parse(fs.readFileSync("./Browserless-Backend/config/config.json", "utf-8"));
+      configObject = JSON.parse(fs.readFileSync("./config/config.json", "utf-8"));
       
      }
   catch (err) {
